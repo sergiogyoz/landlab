e62a7bfbbc2d356627b00e6dd2147de14beec04a
mcflugen <mcflugen@gmail.com>
2016-11-20 17:05:02 -0700

    Inherit from LayerFields, remove repeated methods.

--- landlab/layers/binnedlayerstack.py
+++ landlab/layers/binnedlayerstack.py
@@ -1,23 +1,25 @@
 import numpy as np
 import bisect
 
+from .layers import Layers, LayerFields
 
-class BinnedLayerFields(object):
 
-    def __new__(cls, *args, **kwds):
-        for field in ('f', ) + kwds.get('fields', ()):
-            setattr(cls, field, property(lambda x, field=field: x[field],
-                                         doc=field))
+class BinnedLayerFields(LayerFields):
 
-        return object.__new__(cls)
+    # def __new__(cls, *args, **kwds):
+    #     for field in ('f', ) + kwds.get('fields', ()):
+    #         setattr(cls, field, property(lambda x, field=field: x[field],
+    #                                      doc=field))
 
-    def __init__(self, *args, **kwds):
-        self._fields = dict()
-        for field in kwds.get('fields', ()):
-            self._add_field(field)
+    #     return object.__new__(cls)
 
-        self._fields['f'] = np.empty((self.allocated,
-                                      kwds.get('n_grains', 1) - 1))
+    # def __init__(self, *args, **kwds):
+    #     self._fields = dict()
+    #     for field in kwds.get('fields', ()):
+    #         self._add_field(field)
+
+    #     self._fields['f'] = np.empty((self.allocated,
+    #                                   kwds.get('n_grains', 1) - 1))
 
     def _add_field(self, name, **kwds):
         if name not in self._fields:
@@ -43,25 +45,25 @@ class BinnedLayerFields(object):
                 (bins[0] - self.z[-n_bins]) * array[0]
             ) / (bins[1] - self.z[-n_bins])
 
-    def resize(self, *args, **kwds):
-        """Resize field arrays."""
-        for name, array in self._fields.items():
-            self._fields[name] = np.resize(array, self.allocated)
+    # def resize(self, *args, **kwds):
+    #     """Resize field arrays."""
+    #     for name, array in self._fields.items():
+    #         self._fields[name] = np.resize(array, self.allocated)
 
-    def reduce(self, dz, name):
-        bin_dz = np.diff(self.extract(self.thickness - dz))
-        return np.sum(getattr(self, name)[-len(bin_dz):] * bin_dz / dz)
+    # def reduce(self, dz, name):
+    #     bin_dz = np.diff(self.extract(self.thickness - dz))
+    #     return np.sum(getattr(self, name)[-len(bin_dz):] * bin_dz / dz)
 
-    @property
-    def fields(self):
-        """Names of fields tracked."""
-        return self._fields.keys()
+    # @property
+    # def fields(self):
+    #     """Names of fields tracked."""
+    #     return self._fields.keys()
 
-    def __getitem__(self, name):
-        return self._fields[name][:self._top]
+    # def __getitem__(self, name):
+    #     return self._fields[name][:self._top]
 
 
-class BinnedLayerStack(BinnedLayerFields):
+class BinnedLayerStack(Layers, BinnedLayerFields):
 
     """A stack of layers piled on top of one another.
 
@@ -117,49 +119,49 @@ class BinnedLayerStack(BinnedLayerFields):
 
         super(BinnedLayerStack, self).__init__(n_grains=n_grains, **kwds)
 
-    @property
-    def base(self):
-        """Elevation of the bottom of the column.
+    # @property
+    # def base(self):
+    #     """Elevation of the bottom of the column.
         
-        Examples
-        --------
-        >>> from landlab.layers import BinnedLayerStack
-        >>> layers = BinnedLayerStack()
-        >>> layers.base
-        0.0
-        >>> layers.base += 2.
-        >>> layers.base
-        2.0
-        """
-        return self._z0
+    #     Examples
+    #     --------
+    #     >>> from landlab.layers import BinnedLayerStack
+    #     >>> layers = BinnedLayerStack()
+    #     >>> layers.base
+    #     0.0
+    #     >>> layers.base += 2.
+    #     >>> layers.base
+    #     2.0
+    #     """
+    #     return self._z0
 
-    @base.setter
-    def base(self, new_base):
-        self._z0 = new_base
+    # @base.setter
+    # def base(self, new_base):
+    #     self._z0 = new_base
 
-    @property
-    def thickness(self):
-        return self.z[-1]
+    # @property
+    # def thickness(self):
+    #     return self.z[-1]
 
-    @property
-    def top(self):
-        """Elevation of the top of the column.
+    # @property
+    # def top(self):
+    #     """Elevation of the top of the column.
         
-        Examples
-        --------
-        >>> from landlab.layers import BinnedLayerStack
-        >>> layers = BinnedLayerStack()
-        >>> layers.base, layers.top
-        (0.0, 0.0)
-        >>> layers.top = 2.
-        >>> layers.base, layers.top
-        (2.0, 2.0)
-        """
-        return self.base + self._z[self._top]
+    #     Examples
+    #     --------
+    #     >>> from landlab.layers import BinnedLayerStack
+    #     >>> layers = BinnedLayerStack()
+    #     >>> layers.base, layers.top
+    #     (0.0, 0.0)
+    #     >>> layers.top = 2.
+    #     >>> layers.base, layers.top
+    #     (2.0, 2.0)
+    #     """
+    #     return self.base + self._z[self._top]
 
-    @top.setter
-    def top(self, new_top):
-        self._z0 += new_top - self.top
+    # @top.setter
+    # def top(self, new_top):
+    #     self._z0 += new_top - self.top
 
     @property
     def z(self):
@@ -198,9 +200,9 @@ class BinnedLayerStack(BinnedLayerFields):
     def nlayers(self):
         return self._top
 
-    @property
-    def size(self):
-        return self._top + 1
+    # @property
+    # def size(self):
+    #     return self._top + 1
 
     @property
     def allocated(self):
@@ -219,21 +221,21 @@ class BinnedLayerStack(BinnedLayerFields):
 
         super(BinnedLayerStack, self).resize(newsize)
 
-    def is_empty(self):
-        """Check if the stack has any layers.
+    # def is_empty(self):
+    #     """Check if the stack has any layers.
         
-        Examples
-        --------
-        >>> from landlab.layers import BinnedLayerStack
-        >>> layers = BinnedLayerStack()
-        >>> layers.is_empty()
-        True
+    #     Examples
+    #     --------
+    #     >>> from landlab.layers import BinnedLayerStack
+    #     >>> layers = BinnedLayerStack()
+    #     >>> layers.is_empty()
+    #     True
 
-        >>> layers.add(.1)
-        >>> layers.is_empty()
-        False
-        """
-        return self._top == 0
+    #     >>> layers.add(.1)
+    #     >>> layers.is_empty()
+    #     False
+    #     """
+    #     return self._top == 0
 
     def add(self, dz, **kwds):
         """Add sediment to a column.
@@ -283,68 +285,68 @@ class BinnedLayerStack(BinnedLayerFields):
         self._top = new_top
         self._z[self._top] = new_z
 
-    def lift(self, dz):
-        """Lift the base of the stack."""
-        self.base += dz
+    # def lift(self, dz):
+    #     """Lift the base of the stack."""
+    #     self.base += dz
 
-    def lower(self, dz):
-        """Lower the base of the stack."""
-        self.lift(- dz)
+    # def lower(self, dz):
+    #     """Lower the base of the stack."""
+    #     self.lift(- dz)
 
-    def layer_at(self, z, lower=False):
-        """Find the layer containing a particular elevation.
+    # def layer_at(self, z, lower=False):
+    #     """Find the layer containing a particular elevation.
 
-        Parameters
-        ----------
-        z : float
-            Elevation as measured from the bottom of the stack.
+    #     Parameters
+    #     ----------
+    #     z : float
+    #         Elevation as measured from the bottom of the stack.
 
-        Returns
-        -------
-        int
-            Layer number that contains the elevation.
-        """
-        if z < 0. or z > self.thickness:
-            raise ValueError('elevation is outside the column')
-        if lower:
-            return bisect.bisect_left(self._z[:self._top + 1], z) - 1
-        else:
-            return bisect.bisect_right(self._z[:self._top + 1], z) - 1
+    #     Returns
+    #     -------
+    #     int
+    #         Layer number that contains the elevation.
+    #     """
+    #     if z < 0. or z > self.thickness:
+    #         raise ValueError('elevation is outside the column')
+    #     if lower:
+    #         return bisect.bisect_left(self._z[:self._top + 1], z) - 1
+    #     else:
+    #         return bisect.bisect_right(self._z[:self._top + 1], z) - 1
 
-    def extract(self, start=0, stop=None):
-        """Extract layers from a column.
+    # def extract(self, start=0, stop=None):
+    #     """Extract layers from a column.
 
-        Parameters
-        ----------
-        start : float, optional
-            Starting elevation in stack.
-        stop : float, optional
-            Stopping elevation in stack.
+    #     Parameters
+    #     ----------
+    #     start : float, optional
+    #         Starting elevation in stack.
+    #     stop : float, optional
+    #         Stopping elevation in stack.
 
-        Examples
-        --------
-        >>> from landlab.layers import BinnedLayerStack
-        >>> layers = BinnedLayerStack()
-        >>> layers.add(5.5)
-        >>> layers.z
-        array([ 0. ,  1. ,  2. ,  3. ,  4. ,  5. ,  5.5])
+    #     Examples
+    #     --------
+    #     >>> from landlab.layers import BinnedLayerStack
+    #     >>> layers = BinnedLayerStack()
+    #     >>> layers.add(5.5)
+    #     >>> layers.z
+    #     array([ 0. ,  1. ,  2. ,  3. ,  4. ,  5. ,  5.5])
 
-        >>> layers.extract(.5, 4.2)
-        array([ 0.5,  1. ,  2. ,  3. ,  4. ,  4.2])
+    #     >>> layers.extract(.5, 4.2)
+    #     array([ 0.5,  1. ,  2. ,  3. ,  4. ,  4.2])
 
-        >>> layers.extract(1., 4.)
-        array([ 1.,  2.,  3.,  4.])
-        """
-        if stop is None:
-            stop = self.top
-        start, stop = sorted((start, stop))
-        start = np.maximum(start, 0.)
-        stop = np.minimum(stop, self.thickness)
+    #     >>> layers.extract(1., 4.)
+    #     array([ 1.,  2.,  3.,  4.])
+    #     """
+    #     if stop is None:
+    #         stop = self.top
+    #     start, stop = sorted((start, stop))
+    #     start = np.maximum(start, 0.)
+    #     stop = np.minimum(stop, self.thickness)
 
-        bottom = self.layer_at(start)
-        top = self.layer_at(stop, lower=True) + 1
+    #     bottom = self.layer_at(start)
+    #     top = self.layer_at(stop, lower=True) + 1
 
-        z = self._z[bottom:top + 1].copy()
-        z[0], z[-1] = start, stop
+    #     z = self._z[bottom:top + 1].copy()
+    #     z[0], z[-1] = start, stop
 
-        return z
+    #     return z
