63e700ac101a3fece81ef3c8c56f615a9628459a
mcflugen <mcflugen@gmail.com>
2017-05-09 09:10:27 -0600

    Remove numpy version for install tests.

--- .ci/travis/test-conda-install.sh
+++ .ci/travis/test-conda-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-conda create -n _conda_install python=$TRAVIS_PYTHON_VERSION numpy=$NUMPY_VERSION
+conda create -n _conda_install python=$TRAVIS_PYTHON_VERSION
 source activate _conda_install
 conda install --file=requirements.txt
 conda install 'hdf4>=4.2.12'
