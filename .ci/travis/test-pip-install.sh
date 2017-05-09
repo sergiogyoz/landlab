f580dd46aad2506f3e73e85885c8a6c5a7fa2d59
mcflugen <mcflugen@gmail.com>
2017-05-08 22:00:49 -0600

    Install the tested numpy version.

--- .ci/travis/test-pip-install.sh
+++ .ci/travis/test-pip-install.sh
@@ -2,6 +2,7 @@
 
 conda create -n _pip_install python=$TRAVIS_PYTHON_VERSION
 source activate _pip_install
+pip install numpy=$NUMPY_VERSION
 pip install -r requirements.txt
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   conda install python.app
