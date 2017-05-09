b7ca2fadafa75a880d24c6eb5b0d458871b567ce
mcflugen <mcflugen@gmail.com>
2017-05-08 21:15:54 -0600

    Add pip install test script.

---
+++ .ci/travis/test-pip-install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+conda create -n _pip_install python=$TRAVIS_PYTHON_VERSION
+source activate _pip_install
+pip install -r requirements.txt
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  conda install python.app
+  PYTHON=pythonw
+else
+  PYTHON=python
+fi
+$PYTHON setup.py develop
+cd scripts && $PYTHON ./test-installed-landlab.py
+source deactivate
+conda remove -n _pip_install --all
