66f0cb8532fe8fdeaa50d93e1329c0767a9cefb2
mcflugen <mcflugen@gmail.com>
2017-05-08 22:46:50 -0600

    Install hdf4>=4.2.12 for conda install test.

--- .ci/travis/test-conda-install.sh
+++ .ci/travis/test-conda-install.sh
@@ -3,6 +3,7 @@
 conda create -n _conda_install python=$TRAVIS_PYTHON_VERSION numpy=$NUMPY_VERSION
 source activate _conda_install
 conda install --file=requirements.txt
+conda install 'hdf4>=4.2.12'
 python setup.py develop
 cd scripts && ./test-installed-landlab.py || exit 1
 source deactivate
