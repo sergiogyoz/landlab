e188e841c4cbef0422b2d4dae6a4e889e609e2b1
mcflugen <mcflugen@gmail.com>
2017-05-17 09:28:15 -0600

    Test install from PyPI and Anaconda Cloud.

--- .ci/travis/test-conda-install.sh
+++ .ci/travis/test-conda-install.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
-conda create -n _conda_install python=$TRAVIS_PYTHON_VERSION
-source activate _conda_install
-conda install --file=requirements.txt
-conda install 'hdf4>=4.2.12'
-python setup.py develop
-cd scripts && ./test-installed-landlab.py || exit 1
-source deactivate
-conda remove -n _conda_install --all
+# conda create -n _conda_install python=$TRAVIS_PYTHON_VERSION
+# source activate _conda_install
+# conda install --file=requirements.txt
+# conda install 'hdf4>=4.2.12'
+# python setup.py develop
+# cd scripts && ./test-installed-landlab.py || exit 1
+# source deactivate
+# conda remove -n _conda_install --all
+
+mkdir _testing
+cd _testing
+conda install landlab -c landlab
+python -c 'import landlab; landlab.test()'
--- .ci/travis/test-pip-install.sh
+++ .ci/travis/test-pip-install.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
 
-conda create -n _pip_install python=$TRAVIS_PYTHON_VERSION
-source activate _pip_install
-pip install -r requirements.txt
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  conda install python.app
-  PYTHON=pythonw
-else
-  PYTHON=python
-fi
-$PYTHON setup.py develop
-cd scripts && $PYTHON ./test-installed-landlab.py || exit 1
-source deactivate
-conda remove -n _pip_install --all
+# conda create -n _pip_install python=$TRAVIS_PYTHON_VERSION
+# source activate _pip_install
+# pip install -r requirements.txt
+# if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+#   conda install python.app
+#   PYTHON=pythonw
+# else
+#   PYTHON=python
+# fi
+# PYTHON=python
+# $PYTHON setup.py develop
+# cd scripts && $PYTHON ./test-installed-landlab.py || exit 1
+# source deactivate
+# conda remove -n _pip_install --all
+
+mkdir _testing
+cd _testing
+pip install landlab
+python -c 'import landlab; landlab.test()'
