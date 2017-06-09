a345746a79b0cf0d2372a159b30d75e9a83902ce
mcflugen <mcflugen@gmail.com>
2017-06-09 14:47:47 -0600

    Install for multiple versions of python.

--- .ci/travis/test-conda-install.sh
+++ .ci/travis/test-conda-install.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
 
-PYTHON_VERSION=$1
+install_landlab() {
+  conda create -n _conda_install python=$1
+  source activate _conda_install
 
-conda create -n _conda_install python=$PYTHON_VERSION
-source activate _conda_install
+  mkdir -p _testing
+  cd _testing
 
-# conda install --file=requirements.txt
-# conda install 'hdf4>=4.2.12'
-# python setup.py develop
-# cd scripts && ./test-installed-landlab.py || exit 1
-# source deactivate
-# conda remove -n _conda_install --all
+  conda install landlab -c landlab
 
-mkdir -p _testing
-cd _testing
-conda install landlab -c landlab
-python -c 'import landlab; landlab.test()' || exit -1
+  python -c 'import landlab; landlab.test()' || exit -1
 
-source deactivate
-conda remove -n _conda_install --all
+  source deactivate
+  conda remove -n _conda_install --all
+}
+
+
+PYTHON_VERSIONS=$*
+
+for v in $PYTHON_VERSIONS; do
+  install_landlab $v || exit -1
+done
--- .ci/travis/test-pip-install.sh
+++ .ci/travis/test-pip-install.sh
@@ -1,27 +1,32 @@
 #! /bin/bash
 
-PYTHON_VERSION=$1
 
-conda create -n _pip_install python=$PYTHON_VERSION
-source activate _pip_install
-# pip install -r requirements.txt
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  conda install python.app
-  PYTHON=pythonw
-else
-  PYTHON=python
-fi
+install_landlab() {
+  conda create -n _pip_install python=$1
+  source activate _pip_install
 
-# $PYTHON setup.py develop
-# cd scripts && $PYTHON ./test-installed-landlab.py || exit 1
-# source deactivate
-# conda remove -n _pip_install --all
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    conda install python.app
+    PYTHON=pythonw
+  else
+    PYTHON=python
+  fi
 
-mkdir -p _testing
-cd _testing
-pip install -r requirements.txt
-pip install landlab
-$PYTHON -c 'import landlab; landlab.test()' || exit -1
+  mkdir -p _testing
+  cd _testing
 
-source deactivate
-conda remove -n _pip_install --all
+  pip install -r requirements.txt
+  pip install landlab
+
+  $PYTHON -c 'import landlab; landlab.test()' || exit -1
+
+  source deactivate
+  conda remove -n _pip_install --all
+}
+
+
+PYTHON_VERSIONS=$*
+
+for v in $PYTHON_VERSIONS; do
+  install_landlab $v || exit -1
+done
