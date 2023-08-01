Fix negative excess shear in sediment capacity calculation

Sergio_stuff/playing_around.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
@@ -20,7 +20,7 @@ class setups:
         self.n = shape[0]
         self.m = shape[1]
         self.steepness = steepness
-        self.grid = [0] * (self.n * self.m) 
+        self.grid = [0] * (self.n * self.m)
 
     def line(self, Vertical=False):
         if not Vertical:
@@ -49,9 +49,9 @@ class setups:
 
 
 # %%
-shape = (3, 3)
+shape = (3, 20)
 reach_lenght = 1
-slope = 1  # 0.004
+slope = 0.004  # 0.004
 s = setups(shape, steepness=slope * reach_lenght)
 # landlab grid
 rastergrid = RasterModelGrid(shape=shape, xy_spacing=1)
@@ -77,16 +77,13 @@ print(ngrid["link"].keys())
 print(ngrid["node"].keys())
 
 # %%
-print(ngrid["node"]["bedrock"])
-print(ngrid["node"]["mean_alluvium_thickness"])
-print(ngrid["node"]["sed_capacity"])
-# %%
 t = 0
 # %%
 dt = 1
 t += dt
 # nety._set_boundary_conditions(0,q_up=0.01, t=t, )
 nety.run_one_step(dt)
+
 
 
 # %%
@@ -108,34 +105,51 @@ nety.run_one_step(dt)
 fig1 = plt.figure(1)
 fig2 = plt.figure(2)
 
-xs = list(range(n - 1))
+n = len(ngrid.at_node["sed_capacity"])
+xs = list(range(n))
 dt = 0.001 * 365.25
-year = 100
+year = 0.001
+record_t = 0.001
 sed_source = np.array([0])
-total_time = year * 1000  # in years
-# sedimentograph
+total_time = int(year * 1000)
+# downstream distance
+distance = np.zeros_like(ngrid.at_node["sed_capacity"])
+distance[1:] = np.cumsum(ngrid.at_link["reach_length"])
 
+# sedimentograph initial pulse
 qt = 1
 ngrid.at_node["sed_capacity"][sed_source] = qt
 
 for time in range(total_time):
-    if (time % (10 * year)) == 0:
+#    print(ngrid.at_node["sed_capacity"])
+    print(time)
+    if (time % (1000 * record_t)) == 0:
         plt.figure(fig1)
         bed = ngrid.at_node["bedrock"][xs]
-        plt.plot(xs, bed, label=f"iter {time}")
+        plt.plot(distance, bed, label=f"t= {time/1000:.3f} and i ={time}")
 
         plt.figure(fig2)
         alluvium = ngrid.at_node["mean_alluvium_thickness"][xs]
-        plt.plot(xs, alluvium, label=f"iter {time}")
-    nety.run_one_step(dt=dt, urate=0, omit=sed_source)
+        plt.plot(distance, np.log10(alluvium), label=f"t= {time/1000:.3f} and i ={time}")
+    nety.run_one_step(dt=dt)  #,omit=sed_source)
 
 # %%
 plt.figure(fig1)
+plt.figure(fig1)
+plt.xlabel("downstream distance")
+plt.ylabel("bedrock elevation (m)")
 plt.legend()
 plt.show()
 
 plt.figure(fig2)
+plt.xlabel("downstream distance")
+plt.ylabel("alluvium thickness (m)")
 plt.legend()
 plt.show()
+
+# %%
+print(ngrid["node"]["bedrock"])
+print(ngrid["node"]["mean_alluvium_thickness"])
+print(ngrid["node"]["sed_capacity"])
 
 # %%

landlab/components/Sergio/Componentcita.py
@@ -266,7 +266,7 @@ class Componentcita(Component):
 
         Examples
         --------
-        
+
         """
         super().__init__(grid)
 
@@ -301,8 +301,8 @@ class Componentcita(Component):
             msg = "NetworkSedimentTransporter: grid must be NetworkModelGrid"
             raise ValueError(msg)
 
-        # self._fd = flow_director  # should I assume flow routing happens outside for now
-        # supported flow directors, ommited for now
+        # flow routing should happen outside for now
+        # supported flow directors
         if not isinstance(flow_director, FlowDirectorSteepest):
             msg = (
                 "NetworkSedimentTransporter: flow_director must be "
@@ -324,9 +324,9 @@ class Componentcita(Component):
         # update channel slopes
         self._update_channel_slopes()
         # set conditions at the futher dowstream nodes
-        #  don't know yet how to handle these
+        # don't know yet how to handle these
 
-    def run_one_step(self, dt, urate=-1, omit=np.array([], np.int32)):
+    def run_one_step(self, dt, omit=np.array([], np.int32)):
         """
         It updates the mean alluvium thickness and the bedrock elevation based
         on the model by Zhang et al (2015,2018)
@@ -470,16 +470,23 @@ class Componentcita(Component):
         the upstream node field "sed_capacity". It uses the formula
         bla bla bla and ignores tau star crit
         the real issue is that the threshold of motion doesn't
-        depend on the grain size which is concerning to me...
+        depend on the grain size which concerns me...
         """
         sed_cap_omit = self._grid.at_node["sed_capacity"][omit]
         excess_shear = tau_star - self.sstau_star_c
-        excess_shear[excess_shear < 0] = 0
-        self._grid.at_node["sed_capacity"][self._unode] = (self.ssalpha
-            * ((self.spec_grav * self.G * self._grid.at_link["sediment_grain_size"])**0.5)
+        zeromask = np.ones_like(excess_shear)
+        zeromask[excess_shear < 0] = 0
+        excess_shear = excess_shear * zeromask
+
+        self._grid.at_node["sed_capacity"][self._unode] = (
+            self.ssalpha * zeromask
+            * np.power(self.spec_grav * self.G * self._grid.at_link["sediment_grain_size"], 0.5)
             * self._grid.at_link["sediment_grain_size"]
-            * (excess_shear)**(self.ssna))
+            * np.power(excess_shear, self.ssna))
         self._grid.at_node["sed_capacity"][omit] = sed_cap_omit
+        nanarr = np.isnan(self._grid.at_node["sed_capacity"])
+        if np.any(nanarr):
+            raise ArithmeticError("A shear calculation yield nans")
 
     def _bed_erosion(self, dt):
         """